d27f9da983c7e2450ee9246e0d50466e19fff862
health2wealth <94756047+YashSaxena1012@users.noreply.github.com>
2022-06-11 17:17:51 +0530

    Add files via upload

---
+++ form.js
@@ -0,0 +1,8 @@
+function switch1() {
+    document.querySelector('.login-welcome').style.visibility = "hidden"
+    document.querySelector('.signup-welcome').style.visibility = "visible"
+}
+function switch2() {
+    document.querySelector('.login-welcome').style.visibility = "visible"
+    document.querySelector('.signup-welcome').style.visibility = "hidden"
+}
